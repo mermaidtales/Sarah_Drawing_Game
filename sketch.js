684e2f4d1428e84c2faf6604b4deb9a376823923
mermaidtales <75217755+mermaidtales@users.noreply.github.com>
2021-02-14 14:55:53 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+let x = 100;
+let y = 100;
+
+function setup() {
+  createCanvas(800, 800);
+  stroke(255, 0, 0);
+  fill(255, 0, 0);
+}
+
+function draw() {
+  if (keyIsDown(LEFT_ARROW)) {
+    stroke (255, 0, 0);
+    fill (255, 0, 0);
+  }
+
+  if (keyIsDown(RIGHT_ARROW)) {
+    stroke(0, 226, 255);
+    fill(0, 225, 255);
+  }
+
+  if (keyIsDown(UP_ARROW)) {
+    stroke(255, 255, 0);
+    fill(255, 255, 0);
+  }
+
+  if (keyIsDown(DOWN_ARROW)) {
+    stroke(1, 255, 0);
+    fill(1, 255, 0);
+  }
+
+  //clear();
+  ellipse(mouseX, mouseY, 50, 50);
+  
+}
+
+function mouseClicked () {
+    stroke(255);
+    fill(255);
+  }
